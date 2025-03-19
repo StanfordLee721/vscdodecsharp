8e9e8908fe5f149c6c07967272fc32bbcb8071a1
StanfordLee721 <StanfordLee0721@hotmail.com>
2025-03-19 09:04:57 +0800

    test

---
+++ csharpdemo/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const var IsDecimal = 1000m; //decimal
+        const var IsDouble = 1000d; //double
+        const var IsFloat = 1000f; //float
+        const var IsByte = 0b1101; //二進制
+        const var IsHex = 0xFF;//十六進制
+        const var data1 = 85; //十進制
+        const var data2 = 0213; //八進制
+        const var data3 = 0x4b; //十六進制
+        const var data4 = 30; //int 
+        const var data5 = 30u; //無符號int 
+        const var data6 = 30l; //long 
+        const var data7 = 30ul; //無符號long 
+    }
+}
